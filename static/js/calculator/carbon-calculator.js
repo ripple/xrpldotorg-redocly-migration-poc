c149c30a9399cdfed5f65a12db4d1ff3e2a4d2df
jonathanlei <jonathanlei98@gmail.com>
2022-10-11 16:16:29 -0700

    initial commit - need to fix scripting

--- static/js/calculator/carbon-calculator.js
+++ static/js/calculator/carbon-calculator.js
@@ -1,35 +1,97 @@
-let calculator_data = {
-  'btc': {
-    'kWh': 951.58,
-    'tons': 0.000000466,
-    'gas': 75.7
-  },
-  'eth': {
-    'kWh': 42.86334969,
-    'tons': 0.0000000273454,
-    'gas': 2.38677
-  },
-  'pap': {
-    'kWh': 0.044,
-    'tons': 0.0000000000232,
-    'gas': 0.0035
-  },
-  'xrp': {
-    'kWh': 0.0079,
-    'tons': 0.0000000000045,
-    'gas': 0.00063
-  },
-  'vsa': {
-    'kWh': 0.0008,
-    'tons': 0.00000000000046,
-    'gas': 0.00006
-  },
-  'mst': {
-    'kWh': 0.0006,
-    'tons': 0.00000000000051,
-    'gas': 0.00005
-  },
+const coCash = require("../co2-cash.json");
+const coCredit = require("../co2-credit.json");
+const coCrypto = require("../co2-credit.json");
+const gasCrypto = require("../gas-crypto.json");
+const gasCash = require("../gas-cash.json");
+const gasCredit = require("../gas-credit.json");
+const lottie = require("lottie-web");
+
+function co2CashAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("co2Animation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: coCash,
+  });
 }
+function co2CreditAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("co2Animation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: coCredit,
+  });
+}
+function co2CryptoAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("co2Animation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: coCrypto,
+  });
+}
+function gasCryptoAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("gasAnimation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: gasCrypto,
+  });
+}
+function gasCashAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("gasAnimation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: gasCash,
+  });
+}
+function gasCreditAnimation() {
+  lottie.loadAnimation({
+    container: document.getElementById("gasAnimation"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: gasCredit,
+  });
+}
+let calculator_data = {
+  btc: {
+    kWh: 951.58,
+    tons: 0.000000466,
+    gas: 75.7,
+  },
+  eth: {
+    kWh: 42.86334969,
+    tons: 0.0000000273454,
+    gas: 2.38677,
+  },
+  pap: {
+    kWh: 0.044,
+    tons: 0.0000000000232,
+    gas: 0.0035,
+  },
+  xrp: {
+    kWh: 0.0079,
+    tons: 0.0000000000045,
+    gas: 0.00063,
+  },
+  vsa: {
+    kWh: 0.0008,
+    tons: 0.00000000000046,
+    gas: 0.00006,
+  },
+  mst: {
+    kWh: 0.0006,
+    tons: 0.00000000000051,
+    gas: 0.00005,
+  },
+};
 
 function commarize() {
   // Alter numbers larger than 1k
@@ -37,48 +99,44 @@ function commarize() {
     var units = ["Thousand", "Million", "Billion", "Trillion"];
 
     // Divide to get SI Unit engineering style numbers (1e3,1e6,1e9, etc)
-    let unit = Math.floor(((this).toFixed(0).length - 1) / 3) * 3
+    let unit = Math.floor((this.toFixed(0).length - 1) / 3) * 3;
     // Calculate the remainder
-    var num = (this / ('1e'+unit)).toFixed(2)
-    var unitname = units[Math.floor(unit / 3) - 1]
+    var num = (this / ("1e" + unit)).toFixed(2);
+    var unitname = units[Math.floor(unit / 3) - 1];
 
     // output number remainder + unitname
-    return num + ' ' + unitname
+    return num + " " + unitname;
   }
 
   // return formatted original number
-  return this.toLocaleString()
+  return this.toLocaleString();
 }
 
 // Add method to prototype. this allows you to use this function on numbers and strings directly
-Number.prototype.commarize = commarize
-String.prototype.commarize = commarize
+Number.prototype.commarize = commarize;
+String.prototype.commarize = commarize;
 
-let slider = document.getElementById( 'myRange' ),
-  txns = document.querySelectorAll( '.slider-amt' ),
-  dataTypes = document.querySelectorAll( '.d-output' ),
-  dataTxns = document.querySelectorAll( '.dash' ),
-  dataViz = document.querySelectorAll( '.viz-output' );
+let slider = document.getElementById("myRange"),
+  txns = document.querySelectorAll(".slider-amt"),
+  dataTypes = document.querySelectorAll(".d-output"),
+  dataTxns = document.querySelectorAll(".dash"),
+  dataViz = document.querySelectorAll(".viz-output");
 
 // Update the current slider value (each time you drag the slider handle)
-function doCalculations(val){
-
+function doCalculations(val) {
   // This loops through all the dataTypes returning the calculations for the selected one
-  [].slice.call(dataTypes).forEach( function( dataType ){
-
+  [].slice.call(dataTypes).forEach(function (dataType) {
     // if 'active', do calculations
-    if (dataType.classList.contains('active')){
-
+    if (dataType.classList.contains("active")) {
       // looping through the calculator data
-      let data_type = dataType.getAttribute( 'data-type' ),
-        data_comp = dataType.getAttribute( 'data-comp' ),
-        total =  val * 1000000 * calculator_data[ data_type ][ data_comp ],
-        num = document.getElementById( data_comp + '-' + data_type ),
-
+      let data_type = dataType.getAttribute("data-type"),
+        data_comp = dataType.getAttribute("data-comp"),
+        total = val * 1000000 * calculator_data[data_type][data_comp],
+        num = document.getElementById(data_comp + "-" + data_type),
         // kWhcomp = the number of kWh Portugal used in 2019
-        kWhComp = ( total / 50340000000 ).toFixed( 2 );
+        kWhComp = (total / 50340000000).toFixed(2);
 
-      if (data_comp === 'tons') {
+      if (data_comp === "tons") {
         total = total * 1000000;
       }
 
@@ -86,152 +144,160 @@ function doCalculations(val){
       num.style.transition = "all .2s ease-in-out";
 
       // This is for the kWh comparison, it animates the transition/selection
-      if (data_comp === 'kWh' && kWhComp > .02){
-        if ( kWhComp > 1.1 ) { kWhComp = 1.03 };
-        let dot = document.getElementById(data_comp + '-' + data_type + '-dot');
+      if (data_comp === "kWh" && kWhComp > 0.02) {
+        if (kWhComp > 1.1) {
+          kWhComp = 1.03;
+        }
+        let dot = document.getElementById(data_comp + "-" + data_type + "-dot");
         dot.style.transition = "all .2s ease-in-out";
         dot.style.transform = "scale(" + kWhComp * 100 + ")";
         dot.style.webkitTransform = "scale(" + kWhComp * 100 + ")";
         dot.style.msTransform = "scale(" + kWhComp * 100 + ")";
       }
     }
-  })
+  });
 }
 
 // This function is for the slider to highlight the selected number
-function highlightNum(val){
-  [].slice.call(dataTxns).forEach(function(dataTxn){
-    dataTxn.classList.remove('active');
-    if (dataTxn.dataset.num === val){
-      dataTxn.classList.add('active')
+function highlightNum(val) {
+  [].slice.call(dataTxns).forEach(function (dataTxn) {
+    dataTxn.classList.remove("active");
+    if (dataTxn.dataset.num === val) {
+      dataTxn.classList.add("active");
     }
   });
 }
 
 // Run animations if crypto is selected
-function cryptoSelected(){
+function cryptoSelected() {
   gasCryptoAnimation();
   co2CryptoAnimation();
 }
 // Run animations if cash is selected
-function cashSelected(){
+function cashSelected() {
   gasCashAnimation();
   co2CashAnimation();
 }
 // Run animations if credit is selected
-function creditSelected(){
+function creditSelected() {
   gasCreditAnimation();
   co2CreditAnimation();
 }
 
 // This is function to select the data type
-function changeDataType( type ){
-
-  [].slice.call(dataTypes).forEach( function(dataType){
-    if (dataType.classList.contains(type)){
+function changeDataType(type) {
+  [].slice.call(dataTypes).forEach(function (dataType) {
+    if (dataType.classList.contains(type)) {
       let dType = type.substring(2);
-      dataType.classList.add('active');
-      document.getElementById('gasAnimation').innerHTML = '';
-      document.getElementById('co2Animation').innerHTML = '';
+      dataType.classList.add("active");
+      document.getElementById("gasAnimation").innerHTML = "";
+      document.getElementById("co2Animation").innerHTML = "";
 
-      if (dType === 'cash'){
+      if (dType === "cash") {
         cashSelected();
-      } else if (dType === 'credit'){
-        creditSelected()
+      } else if (dType === "credit") {
+        creditSelected();
       } else {
         cryptoSelected();
       }
     } else {
-      dataType.classList.remove('active');
+      dataType.classList.remove("active");
     }
   });
 
   // based on toggle, then to calculations + slider value
   doCalculations(slider.value);
-
 }
 
-$('.tab-link').on('click', function(e){
+$(".tab-link").on("click", function (e) {
   e.preventDefault();
-  $('#data-selector li').removeClass('active');
-  $(this).parent('li').addClass('active');
+  $("#data-selector li").removeClass("active");
+  $(this).parent("li").addClass("active");
 
   // Pass the type to the Change Data function
   let type = $(this).data("currencytype");
   changeDataType(type);
-  ga( 'send', 'event', 'Carbon Calculator', 'Toggle Data Type', type );
-})
+  ga("send", "event", "Carbon Calculator", "Toggle Data Type", type);
+});
 
 // This is main section that will run the functions once the page is ready
-$(document).ready(function(){
+$(document).ready(function () {
   // gets the slider value
-  [].slice.call(txns).forEach(function(txn){
+  [].slice.call(txns).forEach(function (txn) {
     txn.innerHTML = slider.value;
-  })
+  });
 
   // On the slider change, run the Calculator functions
-  slider.oninput = function() {
+  slider.oninput = function () {
     var val = this.value;
-    [].slice.call(txns).forEach(function(txn){
+    [].slice.call(txns).forEach(function (txn) {
       txn.innerHTML = val;
     });
     highlightNum(val);
     doCalculations(val);
-    ga( 'send', 'event', 'Carbon Calculator', 'Slider Data Amount', val );
-  }
+    ga("send", "event", "Carbon Calculator", "Slider Data Amount", val);
+  };
 
   // On page load, run default functions
   doCalculations(slider.value);
   cryptoSelected();
 
   // For mobile, show or hide the button
-  $(window).on('load resize scroll', function() {
+  $(window).on("load resize scroll", function () {
     if ($(window).width() < 993) {
-      let distance = $('#calculator-outputs').offset().top,
+      let distance = $("#calculator-outputs").offset().top,
         $window = $(window).scrollTop(),
-        data_toggle = $('#calculator-mobile-toggle'),
-        inputs = $('#calculator-inputs'),
-        inputs_offset = $('#calculator-inputs-offset');
+        data_toggle = $("#calculator-mobile-toggle"),
+        inputs = $("#calculator-inputs"),
+        inputs_offset = $("#calculator-inputs-offset");
 
-      if (distance < $window){
-        data_toggle.addClass('show').removeClass('hide');
+      if (distance < $window) {
+        data_toggle.addClass("show").removeClass("hide");
       } else {
-        inputs.removeClass('sticky');
-        data_toggle.addClass('hide').removeClass('show');
-        inputs_offset.removeClass('offset' );
-        $('#data-toggle').text('Change Inputs');
+        inputs.removeClass("sticky");
+        data_toggle.addClass("hide").removeClass("show");
+        inputs_offset.removeClass("offset");
+        $("#data-toggle").text("Change Inputs");
       }
     }
   });
 
   // This is the mobile button
-  $('#calculator-mobile-toggle').on( 'click', function(e){
+  $("#calculator-mobile-toggle").on("click", function (e) {
     e.preventDefault();
 
-    let inputs = $('#calculator-inputs'),
-      inputs_offset = $('#calculator-inputs-offset');
+    let inputs = $("#calculator-inputs"),
+      inputs_offset = $("#calculator-inputs-offset");
 
-    $('#calculator-inputs' ).toggleClass('show');
+    $("#calculator-inputs").toggleClass("show");
 
-    if (inputs.hasClass('sticky')) {
-      inputs.removeClass('sticky');
-      inputs_offset.removeClass('offset');
-      $(this).text('Change Inputs');
-      ga( 'send', 'event', 'Carbon Calculator', 'Mobile Toggle', 'Change Inputs' );
+    if (inputs.hasClass("sticky")) {
+      inputs.removeClass("sticky");
+      inputs_offset.removeClass("offset");
+      $(this).text("Change Inputs");
+      ga(
+        "send",
+        "event",
+        "Carbon Calculator",
+        "Mobile Toggle",
+        "Change Inputs"
+      );
     } else {
-      inputs.addClass('sticky show');
-      inputs_offset.addClass('offset');
-      $(this).text('Hide Inputs');
-      ga( 'send', 'event', 'Carbon Calculator', 'Mobile Toggle', 'Hide Inputs' );
+      inputs.addClass("sticky show");
+      inputs_offset.addClass("offset");
+      $(this).text("Hide Inputs");
+      ga("send", "event", "Carbon Calculator", "Mobile Toggle", "Hide Inputs");
     }
   });
 
-  $(document).on('click', 'a[href^="#calculator"]', function(event) {
+  $(document).on("click", 'a[href^="#calculator"]', function (event) {
     event.preventDefault();
 
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - 80
-    }, 800);
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top - 80,
+      },
+      800
+    );
   });
-  
 });
